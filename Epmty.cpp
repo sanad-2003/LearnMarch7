2dc52d38f5400e44da9510b64bb8daa54e874401
osanad3 <omarmohamedsanad3@gmail.com>
2023-03-07 17:21:17 +0200

    Just changed the name of the file from Emty.cpp to Empty.cpp

---
+++ Epmty.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#define ll long long
+
+using namespace std;
+
+void FAST_IO(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    // freopen("##.in", "r", stdin);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+    #endif
+}
+
+void solve() {
+    
+}
+
+int main() {
+    FAST_IO();
+    int testcases = 1;
+    // cin >> testcases;
+    while (testcases--)
+        solve();
+    return 0;
+}
